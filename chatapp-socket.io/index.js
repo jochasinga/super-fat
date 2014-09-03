007efe42a2c7192004f04b529804e16021f0f72b
Joe Chasinga <jo.chasinga@gmail.com>
2014-09-03 13:29:59 -0400

    Change chatapp port from 3000 to 8000

--- chatapp-socket.io/index.js
+++ chatapp-socket.io/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 /* Firebase Reference */
 
